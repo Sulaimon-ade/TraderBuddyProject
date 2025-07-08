88f882e7ea53ec41346b69632ce4d367a8b00b63
Sulaimon-ade <107030607+Sulaimon-ade@users.noreply.github.com>
2025-07-08 10:29:40 +0100

    testing boostrap before coding

--- TraderBuddy/src/App.tsx
+++ TraderBuddy/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
   return (
     <>
     <div className="card">
-      here
+      <header className="header">
+        this is the header
+      </header>
     </div>
     </>
   )
