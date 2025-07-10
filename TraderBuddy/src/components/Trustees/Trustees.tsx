b413f65d7aeb3588a4249978fb0a9dfb9bb13ca9
Sulaimon-ade <107030607+Sulaimon-ade@users.noreply.github.com>
2025-07-10 14:09:34 +0100

    added media query to Hero section and header

---
+++ TraderBuddy/src/components/Trustees/Trustees.tsx
@@ -0,0 +1,13 @@
+import './trustees.css'
+import './img/trustees.svg'
+
+function Trustees() {
+  return (
+    <>
+        <section className='trustees'>
+          <img src="./img/trustees.svg" alt="img" />
+        </section>
+    </>
+  )
+}
+export default Trustees
